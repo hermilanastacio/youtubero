480c89958a9c6560d2de339ad6257a34b09f7dbd
hermilanastacio <hermilanastacio@pinion.ph>
2020-09-25 17:22:10 +0800

    Updates

---
+++ precache-manifest.319643293b5a8094b6b8e2baa95a16f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8160af44ea20707aab11b79dde17e1d",
+    "url": "/youtubero/index.html"
+  },
+  {
+    "revision": "d09af3bdff434f03ae47",
+    "url": "/youtubero/static/css/main.46961390.chunk.css"
+  },
+  {
+    "revision": "e3ecc7c10edeaee5ac32",
+    "url": "/youtubero/static/js/2.1ced6101.chunk.js"
+  },
+  {
+    "revision": "185c296edb68d04ccc84a4b49fca9cb7",
+    "url": "/youtubero/static/js/2.1ced6101.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d09af3bdff434f03ae47",
+    "url": "/youtubero/static/js/main.daed4c16.chunk.js"
+  },
+  {
+    "revision": "88edfaff143a376ead85",
+    "url": "/youtubero/static/js/runtime-main.8052a3a7.js"
+  },
+  {
+    "revision": "8b26dfb25aec68c522763d8b751b8bc1",
+    "url": "/youtubero/static/media/logo.8b26dfb2.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/youtubero/precache-manifest.319643293b5a8094b6b8e2baa95a16f3.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/youtubero/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
